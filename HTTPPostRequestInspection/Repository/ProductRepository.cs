bbb94264707e34a47c659bec7641c9ec91a7f088
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-06-04 11:06:00 +0100

    Add project files.

---
+++ HTTPPostRequestInspection/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using HTTPPostRequestInspection.Models;
+
+namespace HTTPPostRequestInspection.Repository
+{
+    public class ProductRepository
+    {
+        public List<Product> Products = new List<Product>()
+        {
+            new Product() { Id = 1, Name= "Laptop", Price = 1000, Quantity = 10 },
+            new Product() { Id = 2, Name= "Desktop", Price = 2000, Quantity = 20 },
+
+        };
+
+        // Add a New Product and return the newly Created Product Id
+        public async Task<int> AddProduct(Product product)
+        {
+            //Set the Product Id
+            product.Id = Products.Count + 1;
+
+            Products.Add(product);
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            return product.Id;
+
+        }
+
+        // Get a product by Id
+        public async Task<Product> GetById(int Id)
+        {
+            var product = Products.FirstOrDefault(p => p.Id == Id);
+
+            await Task.Delay(TimeSpan.FromSeconds(3));
+
+            return product!;
+
+        }
+    }
+}
